161b33a2398d91e229287cadecd36e18d21557d6
Salako Oluwadolapo <willdynamics@gmail.com>
2023-02-10 12:29:52 +0100

    Update STT.py

--- STT.py
+++ STT.py
@@ -38,37 +38,31 @@ with col1:
 with col2:
     st.markdown(meta.HEADER_INFO, unsafe_allow_html=True)
     st.markdown(meta.CHEF_INFO, unsafe_allow_html=True)
-    audio_lang = st.selectbox("Choose your audio file language", index=0, options=["English", "French", "German"])
+    audio_lang = st.selectbox("Choose your audio file language", index=0, options=["British English", "American English"])
+    if audio_lang == "British English":
+        audio_model = st.selectbox("Choose your preferred model for transcription", index=2, options=["en-GB_BroadbandModel", "en-GB_Multimedia", "en-GB_NarrowbandModel", "en-GB_Telephony"])
+    elif audio_lang == "American English":
+        audio_model = st.selectbox("Choose your preferred model for transcription", index=2, options=["en-US_BroadbandModel", "en-US_Multimedia", "en-US_NarrowbandModel", "en-US_ShortForm_NarrowbandModel", "en-US_Telephony"])
     
-
-    if audio_lang == "English":
-        model_type = "en-US_BroadbandModel"
-        uploaded_file = st.file_uploader("Choose a file", key='files')
-        if uploaded_file is not None:
-            file = split_tup = os.path.splitext(uploaded_file.name)
-            file = file[1].replace(".", "")
+    uploaded_file = st.file_uploader("Choose a file", key='files')
+    if uploaded_file is not None:
+        file = split_tup = os.path.splitext(uploaded_file.name)
+        file = file[1].replace(".", "")
             #print(file)
-            audio_type = "audio/" + str(file)
-            st.audio(uploaded_file.name, format=audio_type)
-            transcribe = st.button('Transcribe 💡')       
-            if transcribe:
+        audio_type = "audio/" + str(file)
+        st.audio(uploaded_file.name, format=audio_type)
+        transcribe = st.button('Transcribe 💡')       
+        if transcribe:
                 # Insert local mp3 file path in
                 # place of 'LOCAL FILE PATH' 
-                with st.spinner('Transcription in progress...'):
-                    dic = json.loads(
-                        json.dumps(authenticate().recognize(audio=uploaded_file,
-                        content_type=audio_type,   
-                        word_alternatives_threshold=0.9,
-                        smart_formatting = True,
-                        speaker_labels = True,
-                        model=model_type).get_result(), indent=2))
-                    st.success('Transcription Successful!')
+            with st.spinner('Transcription in progress...'):
+                dic = json.loads(json.dumps(authenticate().recognize(audio=uploaded_file, content_type=audio_type, word_alternatives_threshold=0.9, smart_formatting = True, speaker_labels = True, model=model_type).get_result(), indent=2))
+            st.success('Transcription Successful!')
   
 # Stores the transcribed text
-                transcribed_text = ""
-                with st.spinner('Retrieving transcribed text...'):
-                    while bool(dic.get('results')):
-                         transcribed_text = dic.get('results').pop().get('alternatives').pop().get('transcript')+transcribed_text[:]
-                text = st.text_area("Results", transcribed_text, height=300)
-                st.download_button(label="Download Transcript", data=transcribed_text, file_name="transcript.text")
-    
+            transcribed_text = ""
+            with st.spinner('Retrieving transcribed text...'):
+                 while bool(dic.get('results')):
+                     transcribed_text = dic.get('results').pop().get('alternatives').pop().get('transcript')+transcribed_text[:]
+            text = st.text_area("Results", transcribed_text, height=300)
+            st.download_button(label="Download Transcript", data=transcribed_text, file_name="transcript.text")
